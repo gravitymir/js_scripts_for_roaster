3dbe12067b412af3b969e5f9a1967f8d64c98d71
Andrey Sukhodeev <gravitymir@gmail.com>
2024-11-07 01:51:58 +0000

    Update group.js check Hollidays

--- group.js
+++ group.js
@@ -1,3 +1,23 @@
+function checkRowsOnHoliday(kitchen_or_bar) {
+        let conteiner = document.getElementById(`conteiner_${kitchen_or_bar}`);
+        let columns = conteiner.getElementsByClassName("column");
+        let checkArray = [];
+        let holiArray = ["H", "O", "L", "I", "D", "A", "Y"];
+        for (let i = 1; i < columns.length; i++) {
+            let paragrafs = columns[i].getElementsByTagName("p");
+
+            let holi = Array.prototype.find.call(paragrafs, (p) => p.innerHTML != "OFF");
+
+            if (!holi) {
+                for (let j = 0; j < paragrafs.length; j++) {
+                    paragrafs[j].innerHTML = holiArray[j];
+                }
+            }
+
+
+        }
+    }
+
 (function init(){
         document.getElementById("main").style.marginLeft = "0px";
         document.getElementById("main").style.display = "flex";
